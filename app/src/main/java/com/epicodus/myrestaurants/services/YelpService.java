de175690c5f9ab46d571c558eac96599eacfa943
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-05-10 10:10:59 -0700

    Refactor into fragments per lesson plan

--- app/src/main/java/com/epicodus/myrestaurants/services/YelpService.java
+++ app/src/main/java/com/epicodus/myrestaurants/services/YelpService.java
@@ -116,7 +116,7 @@ import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
 public class YelpService {
-    public static void findRestaurants(String location, Callback callback) {
+    public void findRestaurants(String location, Callback callback) {
         String CONSUMER_KEY = Constants.YELP_CONSUMER_KEY;
         String CONSUMER_SECRET = Constants.YELP_CONSUMER_SECRET;
         String TOKEN = Constants.YELP_TOKEN;
---
+++ app/src/main/java/com/epicodus/myrestaurants/ui/SavedRestaurantListFragment.java
@@ -0,0 +1,94 @@
+package com.epicodus.myrestaurants.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.epicodus.myrestaurants.Constants;
+import com.epicodus.myrestaurants.R;
+import com.epicodus.myrestaurants.adapters.FirebaseRestaurantListAdapter;
+import com.epicodus.myrestaurants.models.Restaurant;
+import com.epicodus.myrestaurants.util.OnStartDragListener;
+import com.epicodus.myrestaurants.util.SimpleItemTouchHelperCallback;
+import com.firebase.client.Firebase;
+import com.firebase.client.Query;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SavedRestaurantListFragment extends BaseFragment implements OnStartDragListener {
+
+    private Query mQuery;
+    private Firebase mFirebaseRestaurantRef;
+    private FirebaseRestaurantListAdapter mAdapter;
+    private ItemTouchHelper mItemTouchHelper;
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
+
+
+    public SavedRestaurantListFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mFirebaseRestaurantRef = new Firebase(Constants.FIREBASE_URL_RESTAURANTS);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_saved_restaurant_list, container, false);
+        ButterKnife.bind(this, view);
+        setUpFirebaseQuery();
+        setUpRecyclerView();
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    private void setUpFirebaseQuery() {
+        String userUid = mSharedPreferences.getString(Constants.KEY_UID, null);
+        String location = mFirebaseRestaurantRef.child(userUid).toString();
+        mQuery = new Firebase(location).orderByChild("index");
+    }
+
+    private void setUpRecyclerView() {
+        mAdapter = new FirebaseRestaurantListAdapter(mQuery, Restaurant.class, this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRecyclerView.setAdapter(mAdapter);
+
+        ItemTouchHelper.Callback callback = new SimpleItemTouchHelperCallback(mAdapter);
+        mItemTouchHelper = new ItemTouchHelper(callback);
+        mItemTouchHelper.attachToRecyclerView(mRecyclerView);
+    }
+
+    @Override
+    public void onStartDrag(RecyclerView.ViewHolder viewHolder) {
+        mItemTouchHelper.startDrag(viewHolder);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        String uid = mSharedPreferences.getString(Constants.KEY_UID, null);
+        for (Restaurant restaurant : mAdapter.getItems()) {
+            String pushID = restaurant.getPushId();
+            restaurant.setIndex(Integer.toString(mAdapter.getItems().indexOf(restaurant)));
+            mFirebaseRestaurantRef.child(uid)
+                    .child(pushID)
+                    .setValue(restaurant);
+        }
+    }
+
+
+}
