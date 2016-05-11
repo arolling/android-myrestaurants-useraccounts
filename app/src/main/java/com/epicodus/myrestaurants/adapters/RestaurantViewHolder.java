628f8812c1a355b14221b7bf3111d50e51b299bd
Abby Rolling and Daren Schaad <arolling@gmail.com>
2016-05-11 09:43:52 -0700

    Add landscape alternative layout

--- app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantViewHolder.java
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/RestaurantViewHolder.java
@@ -2,14 +2,19 @@ package com.epicodus.myrestaurants.adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
 import com.epicodus.myrestaurants.models.Restaurant;
 import com.epicodus.myrestaurants.ui.RestaurantDetailActivity;
+import com.epicodus.myrestaurants.ui.RestaurantDetailFragment;
 import com.epicodus.myrestaurants.util.ItemTouchHelperViewHolder;
 import com.squareup.picasso.Picasso;
 
@@ -26,11 +31,12 @@ import butterknife.ButterKnife;
 public class RestaurantViewHolder extends RecyclerView.ViewHolder implements ItemTouchHelperViewHolder {
     private static final int MAX_WIDTH= 400;
     private static final int MAX_HEIGHT = 300;
+    private int mOrientation;
+    private Integer mPosition;
 
-    @Bind(R.id.restaurantImageView)
-    ImageView mRestaurantImageView;
-    @Bind(R.id.restaurantNameTextView)
-    TextView mNameTextView;
+
+    @Bind(R.id.restaurantImageView) ImageView mRestaurantImageView;
+    @Bind(R.id.restaurantNameTextView) TextView mNameTextView;
     @Bind(R.id.categoryTextView) TextView mCategoryTextView;
     @Bind(R.id.ratingTextView) TextView mRatingTextView;
 
@@ -42,18 +48,33 @@ public class RestaurantViewHolder extends RecyclerView.ViewHolder implements Ite
         ButterKnife.bind(this, itemView);
         mContext = itemView.getContext();
         mRestaurants = restaurants;
+        mOrientation = itemView.getResources().getConfiguration().orientation;
+        if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            createDetailFragment(0);
+        }
         itemView.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View v) {
-                int itemPosition = getLayoutPosition();
-                Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
-                intent.putExtra("position", itemPosition + "");
-                intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
-                mContext.startActivity(intent);
+                mPosition = getLayoutPosition();
+                if (mOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+                    createDetailFragment(mPosition);
+                } else {
+                    Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                    intent.putExtra(Constants.EXTRA_KEY_POSITION, mPosition.toString());
+                    intent.putExtra(Constants.EXTRA_KEY_RESTAURANTS, Parcels.wrap(mRestaurants));
+                    mContext.startActivity(intent);
+                }
             }
         });
     }
+    private void createDetailFragment(int position) {
+        RestaurantDetailFragment detailFragment = RestaurantDetailFragment.newInstance(mRestaurants, position);
+        FragmentTransaction ft = ((FragmentActivity) mContext).getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.restaurantDetailContainer, detailFragment);
+        ft.commit();
+
+    }
+
 
     public void bindRestaurant(Restaurant restaurant) {
         Picasso.with(mContext).load(restaurant.getImageUrl()).resize(MAX_WIDTH, MAX_HEIGHT).centerCrop().into(mRestaurantImageView);
